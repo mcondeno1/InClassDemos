7998a599a5f662b1607ffc1ff5f49383ead082bf
Matt Condeno <mcondeno1@studentmail.nait.ca>
2015-09-14 20:28:59 -0600

    New project created
    New project

---
+++ eRestaurantDemo/eRestuarantWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eRestuarantWebsite.Startup))]
+namespace eRestuarantWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
