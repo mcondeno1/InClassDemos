3df11ae0d0cfc866ad7eda32e8f72bb4112bb59d
Matt Condeno <mcondeno1@studentmail.nait.ca>
2015-09-21 20:48:28 -0600

    Setting up the component library and initial entities
    Setting up the component library and initial entities

---
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entities/Table.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+
+namespace eRestaurantSystem.DAL.Entities
+{
+    public class Table
+    {
+        [Key]
+        public int TableID { get; set; }
+        public byte TableNumber { get; set; } //tinyint in sql
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+        
+        //Navigation properties
+        //the Reservations table (sql) is a many to many relationship to the Tables table (sql)
+
+
+        //Sql solves this problem by having an associate table
+        //that has a compound primary key created from Reservations
+        //and Tables.
+
+        //We will not be creating an entity for this associate table
+        //instead we will create an overloaded Map in our DbContext class
+
+        //However, we can still create the virtual navigation property to accomodate this relationship
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
+    }
+}
