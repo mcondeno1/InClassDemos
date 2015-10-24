fd58911e03f5e732ce587d061190a72395678ac0
Matt Condeno <mcondeno1@studentmail.nait.ca>
2015-10-23 21:13:03 -0600

    NewReport
    NewReport

--- eRestaurantDemo/eRestaurantSystem/DAL/POCOs/ReservationDetail.cs
+++ eRestaurantDemo/eRestaurantSystem/DAL/POCOs/ReservationDetail.cs
@@ -12,6 +12,7 @@ namespace eRestaurantSystem.DAL.POCOs
 
         public string CustomerName { get; set; }
         public DateTime ReservationDate { get; set; }
+       
         public int NumberInParty { get; set; }
         public string ContactPhone { get; set; }
         public string ReservationStatus { get; set; }
