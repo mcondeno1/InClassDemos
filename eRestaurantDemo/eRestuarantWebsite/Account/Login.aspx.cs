32f10b75edba1abe2a80fa8a389d4c5dbde6af39
Matt Condeno <mcondeno1@studentmail.nait.ca>
2015-11-25 20:09:52 -0700

    Security ASP.NET Identity - eRestaurant Sample/Demo
    Security ASP.NET Identity - eRestaurant Sample/Demo

--- eRestaurantDemo/eRestuarantWebsite/Account/Login.aspx.cs
+++ eRestaurantDemo/eRestuarantWebsite/Account/Login.aspx.cs
@@ -4,6 +4,8 @@ using System;
 using System.Web;
 using System.Web.UI;
 using eRestuarantWebsite;
+using eRestaurantSystem.BLL.Security;
+using eRestaurantSystem.DAL.Entities.Security;
 
 public partial class Account_Login : Page
 {
