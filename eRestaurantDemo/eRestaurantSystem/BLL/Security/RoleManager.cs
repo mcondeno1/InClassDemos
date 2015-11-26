1b549f3a5d604a695b806308dbd4a0b4cab18eb2
Matt Condeno <mcondeno1@studentmail.nait.ca>
2015-11-25 20:33:08 -0700

    RoleManagerSetup
    RoleManagerSetup

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/Security/RoleManager.cs
@@ -0,0 +1,19 @@
+﻿using eRestaurantSystem.DAL.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.BLL.Security
+{
+    public class RoleManager : RoleManager<IdentityRole>
+    {
+        public RoleManager()
+            : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
